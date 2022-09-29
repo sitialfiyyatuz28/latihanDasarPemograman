834e715409afd7290aef2d4ad7f21b131c462381
Siti Alfiyyatuz Zakiyyah Alawiyyah <114629594+sitialfiyyatuz28@users.noreply.github.com>
2022-09-29 11:18:40 +0700

    aplikasiBiodata.py

---
+++ aplikasiBiodata.py
@@ -0,0 +1,25 @@
+#aplikasi biodata create by Alfi
+
+# memasukan nama
+nama = input ("masukkan nama anda! ")
+
+# print / output nama
+print("Nama anda adalah : ",nama)
+
+# masukan kelas
+kelas = input ("masukkan kelas anda! ")
+
+# print / output kelas
+print("Kelas anda adalah : ",kelas)
+
+# masukan prodi
+prodi = input ("masukkan prodi anda! ")
+
+# print / output prodi
+print("Prodi anda adalah : ",prodi)
+
+# masukan tempat tanggal lahir
+tempat_tanggal_lahir = input ("masukkan tempat tanggal lahir anda! ")
+
+# print / output tempat tanggal lahir
+print("Tempat tanggal lahir anda adalah : ",tempat_tanggal_lahir)
